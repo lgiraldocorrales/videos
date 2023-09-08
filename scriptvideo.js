b9b14917fb2a7a3f5a6d6aa3cf5a36048938f7f5
lgiraldocorrales <84143921+lgiraldocorrales@users.noreply.github.com>
2023-09-07 23:56:21 -0500

    Update scriptvideo.js

--- scriptvideo.js
+++ scriptvideo.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
   // Obtén la referencia al elemento div con la clase "relative"
   var videoContainer = $('.vtex-slider-layout-0-x-videoContainer');
 
-  // Crea una etiqueta 'a' y establece el atributo 'href'
+  // Crea una etiqueta 'a' y establece el atributo 'href' con una URL válida
   var link = $('<a></a>', {
     href: 'https://www.spirito.com.co/wellness-club', // Reemplaza con la URL de destino que deseas
   });
 
-  // Inserta la etiqueta 'a' antes de la etiqueta 'video' dentro del div
-  videoContainer.prepend(link);
+  // Obtén el elemento 'video' dentro del contenedor
+  var videoElement = videoContainer.find('video');
+
+  // Envuelve el elemento 'video' con la etiqueta 'a'
+  videoElement.wrap(link);
 });
